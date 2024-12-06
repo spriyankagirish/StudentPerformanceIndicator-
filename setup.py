1c4bd051b399729f5f245d03c99e0af6ff6d6fdb
spriyankagirish <spriyankagirish@gmail.com>
2024-12-06 20:29:03 +0530

    “setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name='Students Performance Indicator',
+    version='0.0.1',
+    author='priyanka',
+    author_email='spriyankagirish@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn']
+)
